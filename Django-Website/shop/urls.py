39455a51f32ff93dcd5d90707554bdf4f0d9b7c6
Harsh Dhamecha <harshdhamecha10@gmail.com>
2021-01-12 22:27:56 +0530

    initial commit

---
+++ Django-Website/shop/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("search/", views.search, name="Search"),
+    path("report/", views.report, name="Report"),
+   
+    
+
+]
---
+++ Django-Website/shop/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from .models import Product, Contact
+from math import ceil
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+from django.http import HttpResponse
+MERCHANT_KEY = 'Your-Merchant-Key-Here'
+
+
+def index(request):
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
+
+
+def searchMatch(query, item):
+    '''return true only if query matches the item'''
+    if query in item.desc or query in item.product_name or query in item.category or query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+
+def search(request):
+    query = request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+
+        n = len(prod)
+        nSlides = n
+        if len(prod) != 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, "msg": ""}
+    if len(allProds) == 0 or len(query)<4:
+        params = {'msg': "Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
+
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+
+def report(request):
+	return render(request, 'shop/report.html')
+
+
+def contact(request):
+    thank = False
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+        thank = True
+    return render(request, 'shop/contact.html', {'thank': thank})
+
+
